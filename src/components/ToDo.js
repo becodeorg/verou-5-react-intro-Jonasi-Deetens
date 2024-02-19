d67d3b01d17c9a624a366755f3300068adef1ea0
Jonasi-Deetens <jonasideetens@hotmail.com>
2024-02-19 14:27:02 +0100

    Remove test code for dragging functionality

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,42 +1,11 @@
 import React, { useState } from 'react'
 
 function ToDo() {
-    const [dragging, setDragging] = useState(false);
-    const [position, setPosition] = useState({ x: 0, y: 100 });
-  
-    const handleMouseDown = (e) => {
-      setDragging(true);
-    };
-  
-    const handleMouseMove = (e) => {
-      if (dragging) {
-        setPosition({
-          x: e.clientX,
-          y: e.clientY,
-        });
-      }
-    };
-  
-    const handleMouseUp = () => {
-      setDragging(false);
-    };
-  
     return (
-        <li style={{
-            position: 'absolute',
-            top: position.y,
-            left: position.x,
-            cursor: dragging ? 'grabbing' : 'grab',
-            padding: '10px',
-            color: 'white',
-            filter: 'drop-shadow(0 0 4px white)',
-          }}
-          onMouseDown={handleMouseDown}
-          onMouseMove={handleMouseMove}
-          onMouseUp={handleMouseUp}
-        className='todo'>
+        <li className='todo'>
             <input type="checkbox" id="todo" name="todo" />
-            ToDo</li>
+            ToDo
+        </li>
     )
 }
 
